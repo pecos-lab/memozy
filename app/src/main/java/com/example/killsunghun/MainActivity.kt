651ce9d6777e5af77d70150c92b46bcd14699ca1
pecos-lab <pecos6246@gmail.com>
2026-03-16 18:30:49 +0900

    수정기능 추가완료

--- app/src/main/java/com/example/killsunghun/MainActivity.kt
+++ app/src/main/java/com/example/killsunghun/MainActivity.kt
@@ -4,38 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.killsunghun.ui.theme.KillSungHunTheme
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.foundation.clickable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import com.example.killsunghun.ui.theme.KillSungHunTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
 
 
 class MainActivity : ComponentActivity() {
@@ -121,8 +124,11 @@ fun HomeScreen(
                         onNavigateToMemo() // 여기도 교체
                     }, contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = "+", fontSize = 35.sp
+                Image(
+                    painter = painterResource(id = R.drawable.icon_add),
+                    contentDescription = "메모 추가",
+                    colorFilter = ColorFilter.tint(Color.Black),
+                    modifier = Modifier.size(50.dp)
                 )
             }
         }
@@ -153,35 +159,45 @@ fun Greeting(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = name, modifier = Modifier.weight(1f)
+                text = name
             )
 
-            Text(
-                text = killSunghun, modifier = Modifier.weight(1f), textAlign = TextAlign.Center
-            )
-            Text(
-                text = sex, modifier = Modifier.weight(1f), textAlign = TextAlign.End
+            Spacer(modifier = Modifier.weight(1f))
 
-            )
-            //수정버튼
-            Text(
-                text = "수정",
+// 삭제 버튼
+            Box(
                 modifier = Modifier
-                    .padding(start = 10.dp)
-                    .clickable { onEdit() },
-                color = Color.Blue
-            )
-            //삭제버튼
-            Text(
-                text = "삭제",
-                modifier = Modifier
-                    .padding(start = 10.dp)
+                    .size(40.dp)
+                    .border(1.dp, Color.Gray)
                     .clickable { onDelete() },
-                color = Color.Red
-            )
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "삭제",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+// 수정 버튼
+            Box(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(40.dp)
+                    .border(1.dp, Color.Gray)
+                    .clickable { onEdit() },
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "수정",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
