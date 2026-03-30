adf45463e4c60053e959191e990e6e182b4d358c
winter-love-dev <hun230402@gmail.com>
2026-03-30 09:17:47 +0900

    Remove unnecessary code

--- app/src/main/java/me/pecos/memozy/MemozyApplication.kt
+++ app/src/main/java/me/pecos/memozy/MemozyApplication.kt
@@ -2,12 +2,7 @@ package me.pecos.memozy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import net.danlew.android.joda.JodaTimeAndroid
 
 @HiltAndroidApp
-class MemozyApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        JodaTimeAndroid.init(this)
-    }
-}
+class MemozyApplication : Application()
+
